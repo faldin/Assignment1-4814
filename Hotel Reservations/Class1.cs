a8464a5a9a72933d6ad436f571961f1831531c72
Samuel van Dalen <svand014@fiu.edu>
2016-09-15 20:14:25 -0400

    Added Features Class

---
+++ Hotel Reservations/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Reservations
+{
+    public class Features
+    {
+
+        public Boolean laundry { get; set; }
+        public Boolean aircon { get; set; }
+        public Boolean breakfast { get; set; }
+
+        public Features() { }
+
+        public Features(Boolean laundry, Boolean aircon, Boolean breakfast, Boolean shuttle)
+        {
+
+            this.laundry = laundry;
+            this.aircon = aircon;
+            this.breakfast = breakfast;
+            this.shuttle = shuttle;
+
+        }
+    }
+}
